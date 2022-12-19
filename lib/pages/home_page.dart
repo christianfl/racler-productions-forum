f81330893800ca14642692033740a63b07975fea
christianfl <11208965+christianfl@users.noreply.github.com>
2022-12-20 00:52:58 +0100

    Added hint if there are no forum posts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -50,24 +50,41 @@ class _MyHomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: _posts == null ? HomePage._maxPosts : _posts!.length,
-              itemBuilder: (BuildContext context, int index) {
-                if (_posts == null) {
-                  return Shimmer.fromColors(
-                    baseColor: Colors.black12,
-                    highlightColor: Colors.white,
-                    loop: 3,
-                    child: const ForumPost(),
-                  );
-                } else {
-                  return ForumPost(
-                    post: _posts![index],
-                  );
-                }
-              },
-            ),
+            // ignore: prefer_is_empty
+            child: _posts?.length == 0
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.warning,
+                        size: 50,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Kein Post vorhanden. Kommentiere zuerst!',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  )
+                : ListView.builder(
+                    shrinkWrap: true,
+                    itemCount:
+                        _posts == null ? HomePage._maxPosts : _posts!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (_posts == null) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.black12,
+                          highlightColor: Colors.white,
+                          loop: 3,
+                          child: const ForumPost(),
+                        );
+                      } else {
+                        return ForumPost(
+                          post: _posts![index],
+                        );
+                      }
+                    },
+                  ),
           ),
           const SendDialog(),
         ],
